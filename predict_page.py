705703c2a7c14b70c82827fe2aca6522eea134f2
crazystart2002 <133053208+crazystart2002@users.noreply.github.com>
2023-07-14 16:21:26 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def load_model():
-    with open('/Users/admin/PycharmProjects/Survey/Salary_Survey_Regressor-/saved_steps.pkl', 'rb') as file:
+    with open('./saved_steps.pkl', 'rb') as file:
         data = pickle.load(file)
     return data
 
